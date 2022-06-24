4795bd2d7d1ca60b3674825320cb1f92fa7b337b
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2022-06-24 18:06:40 +0200

    remove debug prints

--- movemods.sh
+++ movemods.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-pwd
-ls -al
-
 jq -c '.[]' mods.json |
 while read -r i; do
     mod_repo=$(echo "$i" | jq -cr '.repository')
     mod_name=$(echo "$i" | jq -cr '.name')
 
-    echo $mod_name
-    echo $mod_repo
-
     if [[ ! -f ./"$mod_repo"/"info.json" ]]; then
         mod_repo=$mod_repo/$mod_name
     fi
-
-    echo $mod_name
-    echo $mod_repo
 
     if [[ -d ./"$mod_repo" ]]; then
         mkdir -p ./factorio/mods/"$mod_name"
