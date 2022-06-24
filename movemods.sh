618c544ff0f9895d38cbf75898c484cf98098f59
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2022-06-24 17:59:54 +0200

    Update movemods.sh

--- movemods.sh
+++ movemods.sh
@@ -9,6 +9,10 @@ while read -r i; do
     mod_repo=$(echo "$i" | jq -cr '.repository')
     mod_name=$(echo "$i" | jq -cr '.name')
 
+    if [[ ! -f ./"$mod_repo"/"$info.json" ]]; then
+        mod_repo=$mod_repo/$mod_name
+    fi
+
     if [[ -d ./"$mod_repo" ]]; then
         mkdir -p ./factorio/mods/"$mod_name"
         mv ./"$mod_repo"/* ./factorio/mods/"$mod_name"
