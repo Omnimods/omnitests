440993d9aab21a5d8f1d6b55a7797c833f4b3f6e
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2022-06-30 21:02:51 +0200

    Update movemods.sh

--- movemods.sh
+++ movemods.sh
@@ -2,31 +2,28 @@
 set -euo pipefail
 
 mod_set_name=$1
-
+mod_set_name="$mod_set_name" jq -n 'env.mod_set_name'
 echo "1: $1"
 
-echo "test0"
-jq '.include[]' mod-sets.json
-echo "test01"
-jq '.include[] | .name' mod-sets.json
+
 echo "test1"
 jq '.include[] | select(.name=="pure_omni_qol")' mod-sets.json
 echo "test1"
-jq '.include[] | select(.name=="$mod_set_name")' mod-sets.json
+jq '.include[] | select(.name==$mod_set_name)' mod-sets.json
 
 
-jq '.include[]' mod-sets.json |
-while read -r i; do
-    setname=$(echo "$i" | jq -cr '.name')
-    echo "Setname: $setname"
+# jq '.include[]' mod-sets.json |
+# while read -r i; do
+#     setname=$(echo "$i" | jq -cr '.name')
+#     echo "Setname: $setname"
 
-    if [[ $setname == $mod_set_name ]]; then
-        ref=$EVENT_REF
-        included_mods=$(echo "$i" | jq -cr '.mods')
-        echo "FOUND: $included_mods"
-    fi
+#     if [[ $setname == $mod_set_name ]]; then
+#         ref=$EVENT_REF
+#         included_mods=$(echo "$i" | jq -cr '.mods')
+#         echo "FOUND: $included_mods"
+#     fi
     
-done
+# done
 
 
 
