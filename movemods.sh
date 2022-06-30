193b0ecf4050ee673eed00d87d6f535b48b22d9d
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2022-06-30 22:27:50 +0200

    Update movemods.sh

--- movemods.sh
+++ movemods.sh
@@ -2,34 +2,6 @@
 set -euo pipefail
 
 mod_set_name=$1
-echo "1: $1"
-
-
-echo "test1"
-jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods | index("omnilib")' mod-sets.json
-jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods | index("omnimatter")' mod-sets.json
-echo "test2"
-included_set=$(jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods | index("omnilib")' mod-sets.json)
-echo "$included_set"
-
-
-if [[ "$included_set" != "null" ]]; then
-    echo "in"
-else
-    echo "out"
-fi
-
-echo "test3"
-included_sett=$(jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods | index("test123")' mod-sets.json)
-echo "$included_sett"
-
-
-if [[ "$included_sett" != "null" ]]; then
-    echo "in"
-else
-    echo "out"
-fi
-
 
 jq -c '.[]' mods.json |
 while read -r i; do
@@ -37,7 +9,7 @@ while read -r i; do
     mod_name=$(echo "$i" | jq -cr '.name')
     included_set=$(jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods | index('\"$mod_name\"')' mod-sets.json)
 
-    if [[ ! -f ./"$mod_repo"/"info.json" ]]; then
+    if [[ ! -f ./"$mod_repo"/"info.json" ]] $$ [[ "$included_set" != "null" ]]; then
         mod_repo=$mod_repo/$mod_name
     fi
 
@@ -46,5 +18,4 @@ while read -r i; do
         mv ./"$mod_repo"/* ./factorio/mods/"$mod_name"
         rm -r ./"$mod_repo"
     fi
-
 done
