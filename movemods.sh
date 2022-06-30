478514bcb692075a7e5f176d811bec01f69aca66
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2022-06-30 20:53:53 +0200

    Update movemods.sh

--- movemods.sh
+++ movemods.sh
@@ -8,7 +8,9 @@ echo "1: $1"
 echo "test0"
 jq '.include[]' mod-sets.json
 echo "test1"
-jq ".include[] | select(.name==$mod_set_name)" mod-sets.json
+jq ".include[] | select(.name=='pure_omni_qol')" mod-sets.json
+echo "test1"
+jq ".include[] | select(.name=='$mod_set_name')" mod-sets.json
 
 
 jq '.include[]' mod-sets.json |
