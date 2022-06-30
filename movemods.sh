7da404e14ec9868700ad18d2ee230301070875a4
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2022-06-30 21:34:43 +0200

    Update movemods.sh

--- movemods.sh
+++ movemods.sh
@@ -8,15 +8,26 @@ echo "1: $1"
 echo "test1"
 jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods' mod-sets.json
 echo "test2"
-jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods | index("omnilib")' mod-sets.json
-echo "test3"
-jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods | index("test123")' mod-sets.json
+included_set=jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods | index("omnilib")' mod-sets.json
+if [[ -n included_set]]; then
+    echo "in"
+else
+    echo "out"
+fi
 
+echo "test3"
+included_set=jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods | index("test123")' mod-sets.json
+if [[ -n included_set]]; then
+    echo "in"
+else
+    echo "out"
+fi
 
 jq -c '.[]' mods.json |
 while read -r i; do
     mod_repo=$(echo "$i" | jq -cr '.repository')
     mod_name=$(echo "$i" | jq -cr '.name')
+    
 
     if [[ ! -f ./"$mod_repo"/"info.json" ]] && [[ -n jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods | index('\"$mod_name\"')' mod-sets.json]]; then
         mod_repo=$mod_repo/$mod_name
