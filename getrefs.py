585c8082a3136037df6836c2278fa9ee5645953d
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2024-11-07 20:29:59 +0100

    Update getrefs.py

--- getrefs.py
+++ getrefs.py
@@ -41,7 +41,7 @@ def main():
         else:
             try:
                 branch = repo.get_branch(event_branch)
-            except github.GithubException:
+            except github.GithubException.GithubException:
                 branch = repo.get_branch(repo.default_branch)
             ref = branch.commit.sha
 
