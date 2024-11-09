b911881c34585dc23202868fe3b1d33b2f754b20
Cereal Sam <62474527+Cereal-Sam@users.noreply.github.com>
2024-11-09 13:39:18 +0100

    Update movemods.sh

--- movemods.sh
+++ movemods.sh
@@ -4,14 +4,13 @@ set -euo pipefail
 mod_set_name=$1
 
 echo "mods to move: $(jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods' mod-sets.json)"
-
+use_SA=$(jq '.include[] | select(.name=='\"$mod_set_name\"') | .use_SA' mod-sets.json)
 
 jq -c '.[]' mods.json |
 while read -r i; do
     mod_repo=$(echo "$i" | jq -cr '.repository')
     mod_name=$(echo "$i" | jq -cr '.name')
     included_set=$(jq '.include[] | select(.name=='\"$mod_set_name\"') | .mods | index('\"$mod_name\"')' mod-sets.json)
-    use_SA=$(echo "$i" | jq -cr '.use_SA')
 
     if [[ ! -f ./"$mod_repo"/"info.json" ]] && [[ "$included_set" != "null" ]]; then
         mod_repo=$mod_repo/$mod_name
@@ -23,7 +22,7 @@ while read -r i; do
         mv ./"$mod_repo"/* ./factorio/mods/"$mod_name"
         rm -r ./"$mod_repo"
 
-        if [[ "$use_SA" != "true" ]] && [[ ! -f "./factorio/mods/mod-list.json" ]]; then
+        if [[ "$use_SA" == "true" ]] && [[ ! -f "./factorio/mods/mod-list.json" ]]; then
             echo "Moved mod mod-list.json to disable SA"
             mv "mod-list.json" "./factorio/mods/"
         fi
